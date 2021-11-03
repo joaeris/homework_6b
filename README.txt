a7de7c4d06a280e5f5407a3a5eca4efa6fe8c6f8
joaeris <91208076+joaeris@users.noreply.github.com>
2021-11-03 13:51:11 -0400

    version 1
    New homework

---
+++ README.txt
@@ -0,0 +1,5 @@
+You can now click on the Round Pillow and the Floor Pouf.
+
+I did this for the extra credit.
+
+This project certainly opened my eyes that I have A LOT to improve on in HTML. Nonetheless it was a good challenge. 
---
+++ main.js
@@ -0,0 +1,21 @@
+function rainy() {
+    document.getElementById('orderHere').innerHTML = "Rainy Day";
+  }
+
+  function morningHaze() {
+    document.getElementById('orderHere').innerHTML = "Morning Haze";
+  }
+
+  function afterSpecial() {
+    document.getElementById('orderHere').innerHTML = "After School Special";
+  }
+
+  function denim() {
+    document.getElementById('orderHere').innerHTML = "Denim";
+  }
+
+  function submit() {
+      var select = document.getElementById("quantity")
+      var quantity = select.numbers
+  }
+  
